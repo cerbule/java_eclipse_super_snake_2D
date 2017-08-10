c7bd59daeb0b6d2b9aaf8a3555a14e3f1660aeae
cerbule <alindomuta@gmail.com>
2017-08-11 00:22:31 +0300

    Adding the movement, enemy, score and length also collision and game over text

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,9 +1,11 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -20,7 +22,7 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	private boolean up = false;
 	private boolean down = false;
 	
-	//4 varibales for the snake face
+	//4 variables for the snake face
 	private ImageIcon rightmouth;
 	private ImageIcon leftmouth;
 	private ImageIcon upmouth;
@@ -32,6 +34,20 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	//speed of the snake inside the panel
 	private Timer timer;
 	private int delay = 100;
+	
+	//default position for the pick-up(enemy)
+	private int[] enemyxpos={25,50,75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850};
+	private int[] enemyypos={75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,625};
+	
+	//spawn the enemy randomly from the upper list array
+	private ImageIcon enemyimage;
+	
+	private Random random = new Random();
+	private int xpos = random.nextInt(34);
+	private int ypos = random.nextInt(23);
+	
+	//variables for score and length
+	private int score = 0;
 	
 	//this variable will set the first postition of the snake, when he is not moving
 	private int moves = 0;
@@ -58,7 +74,7 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 		//if you are in a playmode don't check this
 		//executed first time, when the game has just started
 		if(moves == 0){
-			snakexlength[2] = 150;
+			snakexlength[2] = 50;
 			snakexlength[1] = 75;
 			snakexlength[0] = 100;
 			
@@ -83,6 +99,18 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 		g.setColor(Color.BLACK);
 		g.fillRect(25, 75, 850, 575);
 		
+		//draw the score
+		g.setColor(Color.white);
+		g.setFont(new Font("arial", Font.PLAIN, 14));
+		//draw the font and set the position
+		g.drawString("Score: " + score, 780, 30);
+		
+		//draw the length of snake
+		g.setColor(Color.white);
+		g.setFont(new Font("arial", Font.PLAIN, 14));
+		//draw the font and set the position
+		g.drawString("Length: " + lengthofsnake, 780, 50);
+
 		//draw the snake
 		rightmouth = new ImageIcon("rightmouth.png");
 		rightmouth.paintIcon(this, g, snakexlength[0], snakeylength[0]);
@@ -112,18 +140,191 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 			}
 		}
 		
+
+		//draw the enemy
+		enemyimage = new ImageIcon("enemy.png");
+		//detect if the head is colliding with the enemy
+		if(enemyxpos[xpos] == snakexlength[0] && enemyypos[ypos] == snakeylength[0]){
+			score++;
+			//increment the length of snake
+			lengthofsnake++;
+			xpos = random.nextInt(34);
+			ypos = random.nextInt(23);
+		}
+		//draw the enemy
+		enemyimage.paintIcon(this, g, enemyxpos[xpos], enemyypos[ypos]);
+		
+		//detecting the collision of the snake whit itself
+		for(int b = 1; b < lengthofsnake; b++){
+			if(snakexlength[b] == snakexlength[0] && snakeylength[b] == snakeylength[0]){
+				right = false; left = false; up = false; down = false;
+				
+				g.setColor(Color.white);
+				g.setFont(new Font("arial", Font.BOLD, 50));
+				g.drawString("Game Over", 300, 300);
+				
+				g.setFont(new Font("arial", Font.BOLD, 20));
+				g.drawString("Space to RESTART", 350, 340);
+				
+			}
+		}
+		
 		g.dispose();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		timer.start();
+		if(right){
+			
+			for(int r = lengthofsnake - 1; r >=0; r--){
+				//shift the position of snake's head to it's next index
+				snakeylength[r+1] = snakeylength[r];
+			}
+			//shift the position of snake x length
+			for(int r = lengthofsnake; r >=0; r--){
+				if(r == 0){
+					snakexlength[r] = snakexlength[r] + 25;
+				}else{
+					snakexlength[r] = snakexlength[r-1];
+				}
+				//check if the snake is moving and touches the border, he will exit the other side
+				if(snakexlength[r] > 850){
+					snakexlength[r] = 25;
+				}
+			}
+			repaint();
+		}
+		if(left){
+			for(int r = lengthofsnake - 1; r >=0; r--){
+				//shift the position of snake's head to it's next index
+				snakeylength[r+1] = snakeylength[r];
+			}
+			//shift the position of snake x length
+			for(int r = lengthofsnake; r >=0; r--){
+				if(r == 0){
+					snakexlength[r] = snakexlength[r] - 25;
+				}else{
+					snakexlength[r] = snakexlength[r-1];
+				}
+				//check if the snake is moving and touches the border, he will exit the other side
+				if(snakexlength[r] < 25){
+					snakexlength[r] = 850;
+				}
+			}
+			repaint();		
+				}
+		if(up){
+			for(int r = lengthofsnake - 1; r >=0; r--){
+				//shift the position of snake's head to it's next index - x length
+				snakexlength[r+1] = snakexlength[r];
+			}
+			//shift the position of snakey length
+			for(int r = lengthofsnake; r >=0; r--){
+				if(r == 0){
+					snakeylength[r] = snakeylength[r] - 25;
+				}else{
+					snakeylength[r] = snakeylength[r-1];
+				}
+				//check if the snake is moving and touches the border, he will exit the other side
+				if(snakeylength[r] < 75){
+					snakeylength[r] = 625;
+				}
+			}
+			repaint();
+		}
+		if(down){
+			for(int r = lengthofsnake - 1; r >=0; r--){
+				//shift the position of snake's head to it's next index - x length
+				snakexlength[r+1] = snakexlength[r];
+			}
+			//shift the position of snake y length
+			for(int r = lengthofsnake; r >=0; r--){
+				if(r == 0){
+					snakeylength[r] = snakeylength[r] + 25;
+				}else{
+					snakeylength[r] = snakeylength[r-1];
+				}
+				//check if the snake is moving and touches the border, he will exit the other side
+				if(snakeylength[r] > 625){
+					snakeylength[r] = 75;
+				}
+			}
+			repaint();
+		}
 		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		//space to restart
+		if(e.getKeyCode() == KeyEvent.VK_SPACE){
+			moves = 0;
+			score = 0;
+			lengthofsnake = 3;
+			repaint();
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			//added moves++ because if not, the snake will remain allways in default position
+			moves++;
+			
+			right = true;
+			// if the snake goes to left it should not be able to go to left
+			if(!left){
+				right = true;
+			}else{
+				right = false;
+				left = true;
+			}
+			up = false;
+			down = false;					
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			//added moves++ because if not, the snake will remain allways in default position
+			moves++;
+			
+			left = true;
+			// if the snake goes to left it should not be able to go to left
+			if(!right){
+				left = true;
+			}else{
+				left = false;
+				right = true;
+			}
+			up = false;
+			down = false;					
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			//added moves++ because if not, the snake will remain allways in default position
+			moves++;
+			
+			up = true;
+			// if the snake goes to left it should not be able to go to left
+			if(!down){
+				up = true;
+			}else{
+				up = false;
+				down = true;
+			}
+			left = false;
+			right = false;					
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			//added moves++ because if not, the snake will remain allways in default position
+			moves++;
+			
+			down = true;
+			// if the snake goes to left it should not be able to go to left
+			if(!up){
+				down = true;
+			}else{
+				down = false;
+				up = true;
+			}
+			left = false;
+			right = false;					
+		}
 		
 	}
 
